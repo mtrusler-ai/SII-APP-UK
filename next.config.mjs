99271528e226f36bae4ac54dd290ec92839d8bd1
mtrusler-ai <mtrusler@gmail.com>
2025-09-17 00:48:56 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import path from 'path';
-
-/** @type {import('next').NextConfig} */
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const nextConfig = {
   reactStrictMode: false,
   webpack: (config) => {
@@ -11,3 +12,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
